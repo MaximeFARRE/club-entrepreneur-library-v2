25af721a36c02bc430e3416536521def3c8bf155
MaximeFARRE <MaximeFARRE@users.noreply.github.com>
2026-05-29 21:49:34 +0200

    feat: add Mon Espace link to navigation layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -26,6 +26,7 @@ export default async function AppLayout({
               <NavLink href="/emprunter">Emprunter</NavLink>
               <NavLink href="/rendre">Rendre</NavLink>
               <NavLink href="/historique">Historique</NavLink>
+              <NavLink href="/profil">Mon Espace</NavLink>
               <NavLink href="/ajouter">Ajouter</NavLink>
               {isAdmin && <NavLink href="/gerer">Gérer</NavLink>}
             </div>
