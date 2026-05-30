d3d74c74bbee17e8cee04aa90d9048d567493c86
MaximeFARRE <MaximeFARRE@users.noreply.github.com>
2026-05-30 07:39:41 +0200

    chore: add predefined categories list in constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const CATEGORIES_PREDEFINIES = [
+  "Développement personnel",
+  "Entrepreneuriat",
+  "Finance & Investissement",
+  "Marketing & Vente",
+  "Management & Leadership",
+  "Autobiographie & Biographie",
+  "Tech & Innovation",
+  "Stratégie & Économie",
+] as const;
